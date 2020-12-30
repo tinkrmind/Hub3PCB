4708ec52494121cf3d9503d91c6bbbeab30968fd
Amitabh Shrivastava <tinkrmind@gmail.com>
2020-12-30 15:43:14 -0500

    Update test-i2c.py

--- test-i2c.py
+++ test-i2c.py
@@ -7,17 +7,17 @@ i2cbus = SMBus(1)
 i2caddress = 0x32
 
 def reset():
-    i2cbus.write_byte_data(i2caddress, MAX_REG_SOFTWARE_RESET, RST)
+    i2cbus.write_byte_data(i2caddress, MAX98372.MAX_REG_SOFTWARE_RESET, MAX98372.RST)
 
 def enable(enable):
-    i2cbus.write_byte_data(i2caddress, MAX_REG_GLOBAL_ENABLE, ENABLE if enable else 0)
+    i2cbus.write_byte_data(i2caddress, MAX98372.MAX_REG_GLOBAL_ENABLE, MAX98372.ENABLE if enable else 0)
 
 def begin():
     # Program the device to the desired mode of operation.
-    i2cbus.write_byte_data(i2caddress, MAX_REG_PCM_MODE_CONFIG, CHANSZ_16);
-    i2cbus.write_byte_data(i2caddress, 0x14, 0x40);
+    i2cbus.write_byte_data(i2caddress, MAX98372.MAX_REG_PCM_MODE_CONFIG, MAX98372.CHANSZ_16)
+    i2cbus.write_byte_data(i2caddress, 0x14, 0x40)
 
-    i2cbus.write_byte_data(i2caddress, MAX_REG_PCM_SAMPLE_RATE_SETUP, SPK_SR_44_1KHZ);
+    i2cbus.write_byte_data(i2caddress, MAX98372.MAX_REG_PCM_SAMPLE_RATE_SETUP, MAX98372.SPK_SR_44_1KHZ)
 
 
     # Program the digital audio interface inputs and outputs.
@@ -25,15 +25,15 @@ def begin():
 
     # Program the speaker output amplifier gain and set SPK_EN (this bit must only be toggled when EN is low).
 
-    i2cbus.write_byte_data(i2caddress, MAX_REG_SPEAKER_ENABLE, 1);
+    i2cbus.write_byte_data(i2caddress, MAX98372.MAX_REG_SPEAKER_ENABLE, 1)
 
     # Program the PVDD and thermal ADC.
 
-    i2cbus.write_byte_data(i2caddress, MAX_REG_DYNAMIC_GAIN_ENABLE, PVADC_ENABLE);
+    i2cbus.write_byte_data(i2caddress, MAX98372.MAX_REG_DYNAMIC_GAIN_ENABLE, MAX98372.PVADC_ENABLE)
 
-    i2cbus.write_byte_data(i2caddress, MAX_REG_LIMITER_THRESHOLD, 3);
+    i2cbus.write_byte_data(i2caddress, MAX98372.MAX_REG_LIMITER_THRESHOLD, 3)
 
-    i2cbus.write_byte_data(i2caddress, MAX_REG_PCM_RX_ENABLE_A, 1); # channel 0
+    i2cbus.write_byte_data(i2caddress, MAX98372.MAX_REG_PCM_RX_ENABLE_A, 1) # channel 0
 
     # print(i2cbus.read_byte_data(i2caddress, 0x34))
 
@@ -41,4 +41,4 @@ def begin():
 # begin()
 # enable(1)
 
-print(i2cbus.read_byte_data(i2caddress, MAX_REG_REV_ID))
+print(i2cbus.read_byte_data(i2caddress, MAX98372.MAX_REG_REV_ID))
