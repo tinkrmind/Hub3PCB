d714db50b365440f8d3e1549a28d65edf81429ef
Amitabh Shrivastava <tinkrmind@gmail.com>
2021-02-02 12:41:07 -0500

    added time functionality to attiny

--- attiny_wraper.py
+++ attiny_wraper.py
@@ -9,7 +9,16 @@ def readFlags():
     return i2cbus.read_byte_data(i2caddress, attiny.FLAGS)
 
 def readTime():
-    return i2cbus.read_i2c_block_data(i2caddress,attiny.UNIX_TIME,attiny.UNIX_TIME_LENGTH) 
+    time = i2cbus.read_i2c_block_data(i2caddress,attiny.UNIX_TIME,attiny.UNIX_TIME_LENGTH) 
+    return time[0]+(time[1]<<8)+(time[2]<<16)+(time[3]<<24)
+
+def writeTime():
+    import time
+    t = int(time.time())
+    i2cbus.write_byte_data(i2caddress, attiny.UNIX_TIME, t)
+    i2cbus.write_byte_data(i2caddress, attiny.UNIX_TIME, t>>8)
+    i2cbus.write_byte_data(i2caddress, attiny.UNIX_TIME, t>>16)
+    i2cbus.write_byte_data(i2caddress, attiny.UNIX_TIME, t>>24)
 
 
 print(readFlags())
