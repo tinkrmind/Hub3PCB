6bdc1c089220cddd1ff385b34c9bfcf970e8dd0e
Amitabh Shrivastava <tinkrmind@gmail.com>
2021-02-02 13:34:36 -0500

    all functionality added

--- attiny_wraper.py
+++ attiny_wraper.py
@@ -25,9 +25,28 @@ def readVbat():
     Vbat = i2cbus.read_i2c_block_data(i2caddress,attiny.ADC_VBATT,attiny.ADC_VBATT_LENGTH) 
     return (Vbat[0]+(Vbat[1]<<8))
 
+def readIB():
+    ib = i2cbus.read_i2c_block_data(i2caddress,attiny.ADC_IB,attiny.ADC_IB_LENGTH) 
+    return (ib[0]+(ib[1]<<8))
+
+def readVin():
+    VIN = i2cbus.read_i2c_block_data(i2caddress,attiny.ADC_VIN,attiny.ADC_VIN_LENGTH) 
+    return (VIN[0]+(VIN[1]<<8))
+
+def readFanSpeed():
+    return i2cbus.read_byte_data(i2caddress,attiny.FAN_SPEED) 
+    
+def writeFanSpeed(speed):
+    #fan speed in %
+    i2cbus.write_byte_data(i2caddress,attiny.FAN_SPEED, speed) 
 
 print(readFlags())
 print(readTime())
 writeTime()
 print(readTime())
 print(readVbat())
+print(readIB())
+print(readVin())
+print(readFanSpeed())
+writeFanSpeed(50)
+print(readFanSpeed())
